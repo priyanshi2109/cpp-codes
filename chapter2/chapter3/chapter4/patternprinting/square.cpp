3f7af29cb0b7e7d3b3e5edcd33614f3b736e27e0
vishwakarmapriyanshi239-crypto <vishwakarmapriyanshi239@gmail.com>
2026-01-19 18:40:32 +0530

    new repository

--- chapter2/chapter3/chapter4/patternprinting/square.cpp
+++ chapter2/chapter3/chapter4/patternprinting/square.cpp
@@ -1,23 +1,23 @@
 
 
 
-// #include<iostream>
-// using namespace std;
-// int main(){
-//     int m;
-//     cout<<"enter the value of rows: ";//number of rows 
-//     cin>>m;
-//     int n;
-//     cout<<"enter the value of coulmns: ";
-//     cin>>n;
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<<"enter the value of rows: ";//number of rows 
+    cin>>m;
+    int n;
+    cout<<"enter the value of coulmns: ";
+    cin>>n;
 
-//     for(int i=1;i<=m;i++){
-//         for(int j=1;j<=n;j++){
-//             cout<<'*';
-//         }
-//         cout<<endl;
-//     }
-// }
+    for(int i=1;i<=m;i++){
+        for(int j=1;j<=n;j++){
+            cout<<'*';
+        }
+        cout<<endl;
+    }
+}
 
 
 
--- chapter2/chapter3/q6.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main (){
-     
-    for(char c='a';c<='z';c++){
-        cout<<c<<" ";
-    }
-}

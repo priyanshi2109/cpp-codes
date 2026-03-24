beefda49f42c8b13e312efaac778e01ba060e742
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-24 20:01:16 +0530

    arithmetic operations of variable

---
+++ operations.cpp
@@ -0,0 +1,25 @@
+
+
+// INTEGER DATA TYPE - ( integer values are whole numbers that don't  have any decimal or frictional part )
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x =5 ,y=2;
+//     cout<< x+y<<endl;// 5+2=7
+//      cout<< x-y<<endl; // 5-2=3
+//     cout<< x*y<<endl; // 5*2=10
+//     cout<< x/y<<endl; // 5/2 =2  // it returns 2 because it is int data type the it always return int value 
+// }
+
+// FLOAT DATA TYPE (storing real numbers means all numbers inetegers , decimal , fractions  )
+
+#include<iostream>
+using namespace std;
+int main(){
+     float x =5 ,y=2;
+    cout<< x+y<<endl;// 5+2=7
+     cout<< x-y<<endl; // 5-2=3
+    cout<< x*y<<endl; // 5*2=10
+    cout<< x/y<<endl; // 5/2 =2  // it returns 2.5 because it is float data type the it always return int value 
+}

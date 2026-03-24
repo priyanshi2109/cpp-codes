787970c1f5a06d4cb317a192d8474ff860af7a99
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-24 20:14:27 +0530

    increment and decrement of variables

---
+++ increamentdecreament.cpp
@@ -0,0 +1,25 @@
+
+// POST-INCREAMENT 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int x=4;
+// cout<<x<<endl;
+// cout<<x++<<endl;  // first print then increase ( 4,4,5 )
+// cout<<x<<endl;
+// }
+
+// PRE - INCREAMENT
+
+
+#include<iostream>
+using namespace std;
+int main(){
+int x=4;
+cout<<x<<endl;
+cout<<++x<<endl;  // first increase  then print  ( 4 ,5 ,5)
+cout<<x<<endl;
+}
+
+

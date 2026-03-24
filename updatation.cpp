d6e834695e4012176eb946869ba69c600657b7c2
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-24 19:41:31 +0530

    updation of variable

---
+++ updatation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x=5;               // x=5
+    cout<<x<<endl;
+    x = 7;              // x=7
+    cout<<x<<endl;
+    // x = x+6; or 
+    x += 6;             // x= 7+6= 13
+    cout<<x<<endl;
+     x =x-20;          // 13-20= -7
+    cout<<x<<endl;
+}

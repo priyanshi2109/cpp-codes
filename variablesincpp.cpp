5abc3bf0b2a80d959cec1c9d06b5c61874a4b7ee
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-24 19:02:12 +0530

    variables in c++

---
+++ variablesincpp.cpp
@@ -0,0 +1,10 @@
+// VARIABLES
+#include<iostream>
+using namespace std;
+int main(){
+    // this is a comment 
+    int x ;   //declearation
+    x=5;      //initialization
+    int y =9;  // both
+    cout<<x*y;
+}

f52f3c304aeac16414be019f68d456d6b3666929
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-25 17:01:47 +0530

    area of circle

---
+++ areaofcircle.cpp
@@ -0,0 +1,12 @@
+  
+// CALCULATING AREA OF A CIRCLE 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int r= 5;   // radius 
+    float pi = 3.14;
+    int a = pi* r*r; 
+    cout<<" area of circle : "<<a ;
+
+}

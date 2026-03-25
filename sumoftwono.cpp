6ade1c7b16880df26355ff92cb9129522ef4d1ae
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-25 17:03:27 +0530

    sum of two numbers taking user input

---
+++ sumoftwono.cpp
@@ -0,0 +1,14 @@
+// SUM OF two number taking user input 
+
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"emter x: ";
+    cin>>x;
+    int y;
+    cout<<"enter y: ";
+    cin>>y;
+    cout<<"sum of x and y: "<<x+y;
+
+}

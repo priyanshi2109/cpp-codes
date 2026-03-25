8823ac0fca463029fda508a463bf679f53c85097
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-25 17:02:39 +0530

    calculating simple interest

---
+++ simpleinterst.cpp
@@ -0,0 +1,14 @@
+  
+// calculating simple interest 
+
+#include<iostream>
+using namespace std;
+int main(){
+     int p;
+ p=1000;  // the original amount of money 
+ int r = 5;  // the percentage charge per year 
+ int t=3;    // time 3 years 
+ int si = (p*r*t)/100;   // simple interest
+  cout<<" simple inetrest :  "<<si<<endl;
+
+}

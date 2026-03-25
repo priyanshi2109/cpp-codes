b6d6c9680ad9b7a66b29aa8e2b2156604e300659
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-03-25 17:03:56 +0530

    modulus operator

---
+++ modulusopertor.cpp
@@ -0,0 +1,24 @@
+
+//MODULUS OPERATOR( % )
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    // int x=8;               // x%y =x (if x is greater than y)
+    // int y=67;
+    // cout<<x%y<<endl;  //8 
+
+    // int x=8; n            // x%y = 0
+    // int y=8;
+    // cout<<x%y<<endl;  //0 
+     
+
+
+    int x=17;            // x% -y = x% y and -x%y =  -[ x%y]
+    int y= -8;
+    cout<<x% y<<endl;  //0
+
+
+}
+     

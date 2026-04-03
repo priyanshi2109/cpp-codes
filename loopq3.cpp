532664539d68be49c6b1f58ffc51ade09d215727
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-04-03 20:47:00 +0530

    print all even numbers from 1 to 100

---
+++ loopq3.cpp
@@ -0,0 +1,29 @@
+// print all the even numbers from 1 to 100 using loop
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1;i<=100;i++){
+//         if(i%2==0){
+//             cout<<i<<" ";
+//         }
+//     }
+    
+
+// }
+
+
+//       or 
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=2; i<=100;i+=2){
+        cout<<i<<" ";
+    }
+    
+}
+
+
+

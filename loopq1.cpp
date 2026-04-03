dabc4d830ff71c5031ddc431dc55b0ed9f265573
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-04-03 20:37:12 +0530

    print till n numbers taking user input

---
+++ loopq1.cpp
@@ -0,0 +1,12 @@
+// question - wap to print the n  number taking n as user input using for loop
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n :";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        cout<<i;
+    }
+
+}

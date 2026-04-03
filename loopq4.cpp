ff181079cb6b8135ce7b12ef1c32bec7bbed5c6f
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-04-03 20:54:32 +0530

    print the table of any number taking user input

---
+++ loopq4.cpp
@@ -0,0 +1,15 @@
+// print the table of any number taking user input 
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n: ";
+    cin>>n;
+    for(int i=n;i<=n*10;i+=n){
+        cout<<i<<" "<<endl;
+    }
+    
+
+}

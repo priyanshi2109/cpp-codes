564082241f49994b2c772a84aca631937d93e057
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-04-04 19:53:24 +0530

    print 1 to 10

---
+++ whileloopq1.cpp
@@ -0,0 +1,13 @@
+// print the number from 1 to 10 using while loop
+
+#include<iostream>
+using namespace std;
+int main(){
+    int i=1;
+    while(i<=10){
+        cout<<i<<endl;
+        i++;
+    }
+    
+
+}

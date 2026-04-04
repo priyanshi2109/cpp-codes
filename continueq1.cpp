4b9ed451e1043db9b0fbfe9dd7563d70884460f7
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-04-04 19:45:35 +0530

    all odd number from 1 to 100 using continue

---
+++ continueq1.cpp
@@ -0,0 +1,15 @@
+
+// wap to print odd numbers from 1 to 100 using continue statament 
+
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=100;i++){
+        if(i%2==0){
+            continue;
+        }
+        cout<<i<<" ";
+    }
+    
+
+}

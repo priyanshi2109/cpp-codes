871903fb6f71380d3e25aa8cbc850ab4be449b93
Priyanshi Vishwakarma <priyanshi.cs241253@global.org.in>
2026-04-09 19:51:49 +0530

    sum of digits in any number

---
+++ wloopq2.cpp
@@ -0,0 +1,14 @@
+// print the sum of number 
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter n;";
+    cin>>n;
+    int sum=0;
+    while(n!=0){
+        sum=sum+(n%10);
+        n=n/10;
+    }
+    cout<<"sum is "<<sum;
+}
